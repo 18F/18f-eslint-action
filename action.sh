523a68d97a8e99b764990c69e0a2d33633a21c26
Greg Walker <michael.walker@gsa.gov>
2020-10-26 17:28:39 -0500

    add quotes around special characters, for safety

--- action.sh
+++ action.sh
@@ -29,7 +29,7 @@ if [ $3 == "true" ]; then
   # files it doesn't understand and it'll just complain.
   CHANGED=$(
     git diff --diff-filter=ACMR --name-only --relative origin/${GIT_MAIN} -- . |
-    grep -e \.js$ -e \.jsx$ -e \.es6$
+    grep -e "\.js$" -e "\.jsx$" -e "\.es6$"
   )
 
   # Now back to bailing out if there's an error.
